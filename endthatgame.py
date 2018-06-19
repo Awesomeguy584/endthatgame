08a3510b94b2d09e65d5f2f65f6c20130740adc0
Awesomeguy584 <32023664+Awesomeguy584@users.noreply.github.com>
2018-06-19 14:28:26 +0100

    V1.0 Release

--- endthatgame.py
+++ endthatgame.py
@@ -2,8 +2,12 @@ import os
 import subprocess
 import csv
 
+print('EndThatGame V1.0\n')
+
 if os.name == 'nt':
     with open('games.csv', 'r') as games:
         gamesreader = csv.reader(games)
         for line in gamesreader:
             subprocess.run(['taskkill', '/f', '/im', line])
+else:
+    print('OS not supported.')
